919ff2f5885cf2bd4f3e2d0a580cbd75e0374649
wnxlss <sigma1sigmavich1@gmail.com>
2026-07-11 22:14:32 +0300

    Delete models directory

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from models.base import Base
-from models.message import Message
-from models.setting import Setting
-from models.ticket import Ticket, TicketStatus
-from models.user import User
-
-__all__ = ["Base", "User", "Ticket", "TicketStatus", "Message", "Setting"]
